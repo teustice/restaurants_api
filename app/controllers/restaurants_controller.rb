d783fa92358c66f002bf3805336903db527193ae
Tanner Eustice <teustice@msn.com>
2017-07-21 09:22:01 -0700

    add seed for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,4 +12,39 @@ class RestaurantsController < ApplicationController
     # json_response(JSONAPI::Serializer.serialize(@songs , is_collection: true))
   end
 
+  def show
+    @song = Song.find(params[:id])
+    json_response(JSONAPI::Serializer.serialize(@song, include: ['reviews']))
+  end
+
+  def create
+    puts params
+    @song = Song.create!(song_attributes)
+    json_response(JSONAPI::Serializer.serialize(@song), :created)
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update!(song_attributes)
+      json_response({"data":{"type":"messages","attributes":{"message":"The song was updated"}}})
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    if @song.destroy!
+      json_response({"data":{"type":"messages","attributes":{"message":"The song was deleted"}}})
+    end
+  end
+
+  private
+  def song_params
+    params.require(:data).permit(:type, {
+     attributes: [:artist, :title, :id]
+   })
+  end
+
+  def song_attributes
+    song_params[:attributes] || {}
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_restaurants
+  end
+
+  def generate_restaurants
+    50.times do |i|
+      Restaurant.create!(
+        name: Faker::Lorem.word,
+        category: Faker::Lorem.word,
+        address: Faker::Lorem.sentence(5, false, 0).chop,
+        price_range: "Under $#{rand(10...100)}")
+    end
+    p "added #{Restaurant.count} restaurants"
+  end
+end
+
+Seed.begin
